f7da3d0de5d93263f5d099fdeebf8e7a6af55a47
devayani <devayanigandepalli1999@gmail.com>
2024-02-13 15:32:27 +0530

    This is my second commit

---
+++ +ve or _ve.py
@@ -0,0 +1,13 @@
+class num:
+    def pos(self,a):
+        self.a=a
+        if a>0:
+            print("positive")
+        elif a<0:
+            print("negative")
+        else:
+            print("zero")
+a=int(input("enter value"))
+obj=num()
+obj.pos(a)
+
